09996505d72e1c5b4ae3a73471e4f3594b752718
Sean McPheeters <alexissean@Seans-MacBook-Pro.local>
2018-02-27 15:37:18 -0800

    API Homework

---
+++ assets/javascript/main.js
@@ -0,0 +1,94 @@
+$(document).ready(function(){
+
+    var seaAnimals = ["Sharks", "Jelly Fish", "Crabs", "Turtle", "Lobster", "Otter", "Sea Lions", "Eel", "Walrus", "Seahorse"  ];
+
+    // Function that displays all gif buttons
+    
+    function displayGifButtons (){
+        
+        $("gifButtonsView").empty();
+
+        for (var i = 0; i < seaAnimals.length; i++){
+            
+            var gifButton = $("<button>");
+            gifButton.addClass("action");
+            gifButton.addClass("btn btn-primary")
+            gifButton.attr("data-name", seaAnimals[i]);
+            gifButton.text(seaAnimals[i]);
+            $("#gifButtonView").append(gifButton);
+        }
+    }
+
+    // Function to add a new action button
+    
+    function addNewButton(){
+
+        $("#addGif").on("click", function(){
+
+            var action = $("#action-input").val().trim();
+            if (action == ""){
+                return false;
+            }
+
+            seaAnimals.push(action);
+
+            displayGifButtons();
+            return false;
+        });
+    }
+
+    function displayGifs()
+        var action = $(this).attr("#data-name");
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + seaAnimals + "api_key=G91eYzmzipgfpHqVDZAJzEMjBYf68hXh&limit=10";
+        console.log(queryURL);
+
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        })
+
+        .done(function(response){
+            console.log(response);
+            $("#gifsView").empty();
+            var results = response.data;
+            if (results == ""){
+                alert("There isn't a gif for this selection button")
+            }
+
+            for (var i = 0; i < results.length; i++){
+
+                var gifDiv = $("<div>");
+                gifDiv.addClass("gifDiv");
+
+                var gifRating = $("<p>").text("Rating: " + results[i].rating);
+                gifDiv.append(gifRating);
+
+                var gifImage = $("<img>");
+                gifImage.attr("src", results[i].images.fixed_height_small_still.url);
+                gifImage.attr("data-still",results[i].images.fixed_height_small_still.url); 
+                gifImage.attr("data-animate",results[i].images.fixed_height_small.url); // 
+                gifImage.attr("data-state", "still"); 
+                gifImage.addClass("image");
+                gifDiv.append(gifImage);
+
+                $("#gifViews").prepend(gifDiv)
+            }
+        });
+    }
+
+    displayGifButtons();
+    addNewButton();
+    removeLastButton();
+
+    $(document).on("click", ".action", displayGifs)
+    $(document).on("click", ".image", function(){
+        var state = $(this).attr('data-state');
+        if ( state == 'still'){
+            $(this).attr('src', $(this).data('animate'));
+            $(this).attr('data-state', 'animate');
+        }else{
+            $(this).attr('src', $(this).data('still'));
+            $(this).attr('data-state', 'still');
+        }
+    });
+})
